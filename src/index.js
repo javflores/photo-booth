4a67264ea98a22f239b1311ae3a807558180d92c
javflores <juanantoniovicaria@gmail.com>
2017-10-10 05:47:57 +0100

    Add fontawesome and bootstrap as npm packages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import './vendor/font-awesome/css/font-awesome.min.css';
-import './vendor/creative.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'startbootstrap-creative/css/creative.min.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
